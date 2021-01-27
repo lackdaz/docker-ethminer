13c239c742f2013107e359b03e44d9994147d88e
Seth Loh <lackdaz@gmail.com>
2021-01-27 22:44:52 +0800

    added multi gpu support

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker run -e WORKER_NAME=shadow \
-    --name rig_worker1 \
+docker run -e WORKER_NAME=farm1 \
+    --name farm1 \
     --runtime nvidia --gpus all \
     --restart unless-stopped \
     -e ETH_WALLET=0xF65F7b0A01fE59DF50e284Fd21cAEb8Ce5E8746D \
